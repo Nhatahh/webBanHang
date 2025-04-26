ff2af2c2c8b10893ac3e0afdc645cdd699592405
Nhat <nhatvuive123@gmail.com>
2025-04-26 14:12:37 +0700

    lan 13

--- resources/views/users/dangnhap.blade.php
+++ resources/views/users/dangnhap.blade.php
@@ -1,213 +1,81 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css" />
-    <link
-      rel="stylesheet"
-      href="/bootstrap/bootstrap-icons-1.11.3/font/bootstrap-icons.css"
-    />
-    <link rel="stylesheet" href="/public_html/css/style1.css" />
-    <script src="/bootstrap/js/bootstrap.bundle.min.js"></script>
+@extends('layouts.body_user')
 
-    <title>Home</title>
-  </head>
-  <body>
-    <header class="sticky-top">
-      <div class="marquee-container" style="background-color: white;">
-        <marquee behavior="scroll" direction="left"
-          >🔥 Giảm giá sốc 30% toàn bộ sản phẩm! Mua ngay kẻo lỡ! 🚀</marquee
-        >
-      </div>
-      <nav class="navbar navbar-expand-lg navbar-dark">
-        <div class="container-fluid">
-          <a class="navbar-brand fs-1" href="../index.html">
-            <img
-              src="/public_html/images/logo2.png"
-              alt="logo"
-              width="50"
-              height="50"
-              class="d-inline-block align-text-top"
-            />
-            NHATAHH
-          </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarScroll"
-            aria-controls="navbarScroll"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
+@section('title', 'Trang Chủ')
+
+@section('content')
+<!-- Body -->
+  <div class="content">
+    <div class="container-fluid mt-4" style="height: auto">
+      <div class="d-flex flex-column align-items-center">
+        <div class="d-flex justify-content-evenly w-100 w-md-50 w-lg-25 mt-5">
+          <a
+            href="{{ route("user.dangnhap") }}"
+            class="signup active"
+            style="text-decoration: none; color: #002ffe"
+            ><h2 class="mb-4">ĐĂNG NHẬP</h2></a
           >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarScroll">
-            <ul
-              class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll"
-              style="--bs-scroll-height: 100px"
-            >
-              <li class="nav-item">
-                <a class="nav-link" href="../pages/tatcasanpham.html">
-                  SẢN PHẨM <i class="bi bi-chevron-compact-down"></i
-                ></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="../pages/ao.html">
-                  ÁO NAM <i class="bi bi-chevron-compact-down"></i
-                ></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="../pages/quan.html">
-                  QUẦN NAM <i class="bi bi-chevron-compact-down"></i
-                ></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="../pages/membership.html">
-                  MEMBERSHIP
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="../pages/chinhsach.html">
-                  CHÍNH SÁCH
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="../pages/phuongthucthanhtoan.html">
-                  PHƯƠNG THỨC THANH TOÁN
-                </a>
-              </li>
-            </ul>
-            <form class="d-flex">
-              <div class="input-group">
-                <input class="form-control" placeholder="Search" />
-                <button class="btn btn-outline-light" type="submit">
-                  <i class="bi bi-search"></i>
-                </button>
-              </div>
-            </form>
-            <div class="nav-icons mx-3">
-              <a href="../pages/giohang.html"><i class="bi bi-bag"></i></a>
-              <a href="../pages/dangnhap.html"
-                ><i class="bi bi-person-circle"></i
-              ></a>
-            </div>
-          </div>
+          <a
+            href="{{ route("user.dangky") }}"
+            class="signup"
+            style="text-decoration: none; color: #1c77ff"
+            ><h2 class="mb-4">ĐĂNG KÝ</h2></a
+          >
         </div>
-      </nav>
-    </header>
-    <div class="content">
-      <div class="container-fluid mt-4" style="height: auto">
-        <div class="d-flex flex-column align-items-center">
-          <div class="d-flex justify-content-evenly w-100 w-md-50 w-lg-25 mt-5">
-            <a
-              href="{{ route("user.dangnhap") }}"
-              class="signup active"
-              style="text-decoration: none; color: #002ffe"
-              ><h2 class="mb-4">ĐĂNG NHẬP</h2></a
-            >
-            <a
-              href="{{ route("user.dangky") }}"
-              class="signup"
-              style="text-decoration: none; color: #1c77ff"
-              ><h2 class="mb-4">ĐĂNG KÝ</h2></a
-            >
-          </div>
-          <hr class="w-50" />
-          <div class="col-12 col-md-6 col-lg-4">
-            <form action="{{ route('user.xulydangnhap') }}" method="POST" novalidate>
-              @csrf
+        <hr class="w-50" />
+        <div class="col-12 col-md-6 col-lg-4">
+          <form action="{{ route('user.xulydangnhap') }}" method="POST" novalidate>
+            @csrf
+            <input
+              class="form-control mb-3"
+              type="email"
+              name="email"
+              value="{{ old('email') }}"
+              placeholder="* Địa Chỉ Email"
+            />
+            @error('email')
+              <p id="error-email" class="text-danger mb-3 d-flex justify-content-end" style="font-size: 12px; font-style: italic;">{{ $message }}</p>
+            @enderror
+            <input
+              class="form-control mb-3"
+              type="password"
+              name="password"
+              value="{{ old('password') }}"
+              placeholder="* Mật Khẩu"
+            />
+            @error('password')
+              <p id="error-password" class="text-danger mb-3 d-flex justify-content-end" style="font-size: 12px; font-style: italic;">{{ $message }}</p>
+            @enderror
+            <div class="d-flex justify-content-between mb-3">
+              <a href="#" style="text-decoration: none"
+                ><small id="quenMK" class="text-muted"
+                  >Quên mật khẩu?</small
+                ></a
+              >
+              <small class="text-muted">* Bắt buộc</small>
+            </div>
+            <div class="form-check mb-3">
               <input
-                class="form-control mb-3"
-                type="email"
-                name="email"
-                value="{{ old('email') }}"
-                placeholder="* Địa Chỉ Email"
+                class="form-check-input"
+                type="checkbox"
+                value="1"
+                id="flexCheckDefault"
               />
-              @error('email')
-                <p id="error-email" class="text-danger mb-3 d-flex justify-content-end" style="font-size: 12px; font-style: italic;">{{ $message }}</p>
-              @enderror
-              <input
-                class="form-control mb-3"
-                type="password"
-                name="password"
-                value="{{ old('password') }}"
-                placeholder="* Mật Khẩu"
-              />
-              @error('password')
-                <p id="error-password" class="text-danger mb-3 d-flex justify-content-end" style="font-size: 12px; font-style: italic;">{{ $message }}</p>
-              @enderror
-              <div class="d-flex justify-content-between mb-3">
-                <a href="#" style="text-decoration: none"
-                  ><small id="quenMK" class="text-muted"
-                    >Quên mật khẩu?</small
-                  ></a
-                >
-                <small class="text-muted">* Bắt buộc</small>
-              </div>
-              <div class="form-check mb-3">
-                <input
-                  class="form-check-input"
-                  type="checkbox"
-                  value="1"
-                  id="flexCheckDefault"
-                />
-                <small
-                  class="form-check-label text-muted"
-                  for="flexCheckDefault"
-                >
-                  Nhớ mật khẩu
-                </small>
-              </div>
-                <button
-                  type="submit"
-                  class="btn btn-outline-primary btn-lg w-100"
-                >
-                  Đăng Nhập
-                </button>
-            </form>
-          </div>
+              <small
+                class="form-check-label text-muted"
+                for="flexCheckDefault"
+              >
+                Nhớ mật khẩu
+              </small>
+            </div>
+              <button
+                type="submit"
+                class="btn btn-outline-primary btn-lg w-100"
+              >
+                Đăng Nhập
+              </button>
+          </form>
         </div>
       </div>
     </div>
-    <footer>
-      <hr />
-      <div class="row">
-        <div
-          class="col-12 col-md-6 mb-3 d-flex flex-column justify-content-around align-items-center"
-        >
-          <div class="contact fw-bold">
-            <a href="tel:0372576944">📞 0372576944</a><br />
-            <a href="mailto:bhnhatktpm2211005@student.ctuet.edu.vn"
-              >📧 bhnhatktpm2211005@student.ctuet.edu.vn</a
-            >
-          </div>
-          <div class="social-icons">
-            <a href="https://www.facebook.com/huunhat49" target="_blank"
-              ><i class="bi bi-facebook fs-3"></i
-            ></a>
-            <a href="https://www.instagram.com/nhatahh/" target="_blank"
-              ><i class="bi bi-instagram fs-3"></i
-            ></a>
-            <a href="https://www.tiktok.com/@hnhaajt" target="_blank"
-              ><i class="bi bi-tiktok fs-3"></i
-            ></a>
-          </div>
-        </div>
-        <div class="col-12 col-md-6 mb-3 logo d-flex justify-content-around">
-          <img
-            src="/public_html/images/logo2.png"
-            alt="logo"
-            width="150vh"
-            height="150vh"
-          />
-        </div>
-      </div>
-      <hr />
-      <div class="text-center fst-italic mt-3 mb-3">
-        Copyright © 2025 NHATAHH by Bùi Hữu Nhật
-      </div>
-    </footer>
-  </body>
-</html>
+  </div>
+@endsection
